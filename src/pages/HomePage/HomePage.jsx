9c296302d20a17196c2f0691b60eb17142513804
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-09-11 00:30:10 +0300

    css, img

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,27 @@
+import { NavLink } from 'react-router-dom';
+import Phone from '../HomePage/Images/phone.jpg';
+import styles from './HomePage.module.css';
+
+
 const HomePage = () => {
-    return <div></div>;
-  };
-  
-  export default HomePage;
+  return (
+    <main>
+      <section className={styles.container}>
+        <div class="animate__animated animate__heartBeat">
+          <h1 data-text="My contacts">My contacts</h1>
+        </div>
+
+        <div className={styles.thumb}>
+          <div>
+            <img className={styles.image} src={Phone} alt="" />
+          </div>
+        </div>
+        <NavLink to="/contacts" className={styles.button} title="Try it now!">
+          Try it now!
+        </NavLink>
+      </section>
+    </main>
+  );
+};
+
+export default HomePage;
